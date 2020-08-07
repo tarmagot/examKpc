0bcca94bac83e21746ea041e2e70c0ebbcf6ddb4
tarmagot <p.paponchai@gmail.com>
2020-08-07 21:22:21 +0700

    learn redux success

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import React from "react";
+import Action from "./actions";
+import Counter from "./components/counter";
+import { connect } from "react-redux";
 
 import "./App.css";
 
-function App() {
-  return <h1>test creat app</h1>;
+function App(prop) {
+  return (
+    <div className="App">
+      <Counter
+        value={prop.counter}
+        onIncrement={() => prop.increment()}
+        onDecrement={() => prop.decrement()}
+      />
+    </div>
+  );
 }
+const mapStateToProps = (state) => ({
+  counter: state.counter,
+});
 
-export default App;
+const mapDispatchToProps = (dispatch) => ({
+  increment: () =>
+    dispatch({ type: Action.INCREMENT, text: "INCREMENT Redux" }),
+  decrement: () =>
+    dispatch({ type: Action.DECREMENT, text: "DECREMENT Redux" }),
+});
+export default connect(mapStateToProps, mapDispatchToProps)(App);
